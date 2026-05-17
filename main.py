c840da2847a1ed6bade9ad2d0168b94cce07017a
Arafat Hasan <nayemarafat786@gmail.com>
2026-05-17 21:06:22 +0600

    Gemini API for feasability, token mata data.

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import os
+import argparse
+
+from dotenv import load_dotenv
+from google import genai
+
+
+
 def main():
-    print("Hello from snub!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+    
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.user_prompt,
+    )
+
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
